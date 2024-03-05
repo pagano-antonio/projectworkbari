7a536cf329ed6612b855d2f98217ae0cf55e78c0
Jeiempi <jessicamanuelapizzo@hotmail.com>
2024-03-05 17:13:54 +0100

    rest e ctr jessy

--- ProjectWorkBariBE/src/main/java/com/ctr/JobInterviewCtr.java
+++ ProjectWorkBariBE/src/main/java/com/ctr/JobInterviewCtr.java
@@ -1,5 +1,16 @@
 package com.ctr;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dao.JobInterviewRepository;
+
+@Controller
+@RequestMapping("/JobInterviewCtr")
 public class JobInterviewCtr {
+
+	@Autowired
+	private JobInterviewRepository jobInterviewRep;
 
 }
--- ProjectWorkBariBE/src/main/java/com/rest/JobInterviewRest.java
+++ ProjectWorkBariBE/src/main/java/com/rest/JobInterviewRest.java
@@ -1,5 +1,15 @@
 package com.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dao.JobInterviewRepository;
+
+@RestController
+@RequestMapping("JobInterview")
 public class JobInterviewRest {
+	@Autowired
+	private JobInterviewRepository jobInterviewRep;
 
 }
