f9b514bb8abc1ac6346d13e23fd8c2f097b0fdb7
MaryPaolini <marylu.maryu@gmail.com>
2024-03-05 18:00:40 +0100

    metodi

--- ProjectWorkBariBE/src/main/java/com/rest/SkillRest.java
+++ ProjectWorkBariBE/src/main/java/com/rest/SkillRest.java
@@ -1,10 +1,17 @@
 package com.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dao.SkillRepository;
+import com.model.Skill;
 
 
 
@@ -13,5 +20,32 @@ import com.dao.SkillRepository;
 public class SkillRest {
 	@Autowired
     private SkillRepository skillRep;
+	
 
+	@GetMapping("findById/{id}")
+    public Skill findById(@PathVariable ("id") int id) {
+
+    Skill resSkill = skillRep.findById(id).get();
+    return resSkill;
+    }
+	
+	@PostMapping
+	public String post (@RequestBody Skill s) {
+	    System.out.println(s.toString());
+	    skillRep.save(s);
+	    return "success";
+	}
+	
+	@PutMapping
+    public String put (@RequestBody Skill s) {
+    skillRep.save(s);
+       System.out.println(s.toString() + "uploaded");
+       return "success";
+   } 
+
+	@DeleteMapping ("{id}")
+	   public String delete (@RequestBody Skill s) {
+	    skillRep.delete(s);
+	       return s + "deleted";
+	   }
 }
--- ProjectWorkBariBE/src/main/java/com/rest/WorkExperienceRest.java
+++ ProjectWorkBariBE/src/main/java/com/rest/WorkExperienceRest.java
@@ -1,10 +1,16 @@
 package com.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dao.WorkExperienceRepository;
+import com.model.WorkExperience;
 
 
 
@@ -12,5 +18,28 @@ import com.dao.WorkExperienceRepository;
 @RequestMapping("WorkExperience")
 public class WorkExperienceRest {
 	@Autowired
-    private WorkExperienceRepository stateJobInterviewRep;
+    private WorkExperienceRepository workExperienceRep;
+	
+
+	@GetMapping("findById/{id}")
+    public WorkExperience findById(@PathVariable ("id") int id) {
+
+		WorkExperience resWorkExperience = workExperienceRep.findById(id).get();
+        return resWorkExperience;
+    }
+	
+	@PostMapping
+	public String post (@RequestBody WorkExperience s) {
+	    System.out.println(s.toString());
+	    workExperienceRep.save(s);
+	    return "success";
+	}
+	
+	@PutMapping
+    public String put (@RequestBody WorkExperience s) {
+		workExperienceRep.save(s);
+        System.out.println(s.toString() + "uploaded");
+        return "success";
+   } 
+
 }
