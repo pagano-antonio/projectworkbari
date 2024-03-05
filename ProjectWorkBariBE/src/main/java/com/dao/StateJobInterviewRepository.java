6b30f04c0a7bb667e69a3dcaeea9b984f3da4fa8
MaryPaolini <marylu.maryu@gmail.com>
2024-03-05 17:27:57 +0100

    modifica

--- ProjectWorkBariBE/src/main/java/com/dao/StateJobInterviewRepository.java
+++ ProjectWorkBariBE/src/main/java/com/dao/StateJobInterviewRepository.java
@@ -1,11 +1,12 @@
 package com.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.model.StateJobInterview;
 
 
-
+@Repository
 public interface StateJobInterviewRepository extends JpaRepository<StateJobInterview, Integer>{
 
 }
