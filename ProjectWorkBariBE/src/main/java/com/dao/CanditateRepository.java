89bf38feff16e8d080bbaa154c62a9735eb9d3be
Noemi990 <noemidenaro@hotmail.it>
2024-03-08 16:01:46 +0100

    aggiornamento repository query

--- ProjectWorkBariBE/src/main/java/com/dao/CanditateRepository.java
+++ ProjectWorkBariBE/src/main/java/com/dao/CanditateRepository.java
@@ -1,5 +1,8 @@
 package com.dao;
 
+import java.math.BigInteger;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,14 @@ import com.model.Candidate;
 
 @Repository
 public interface CanditateRepository extends JpaRepository<Candidate, Integer> {
-
+	
+	List<Candidate> findBySurname(String surname);
+	List<Candidate> findByCity(String city);
+	List<Candidate> findByPhone(BigInteger phone);
+	List<Candidate> findByCandidateSkills_Skill_Description(String description);
+	List<Candidate> findByEducations_EducationDegreeType_IdEducationDegreeType(Integer idEducationType);
+	List<Candidate> findByJobInterviews_StateJobInterview_Title(String description);//da chiedere se title o description
+	List<Candidate> findByJobInterviews_Outcome(Integer outcome);//proposta stipendiale per quel lavoro(?)
+//lunedi da fare il collegamento impossibile che non sa neanche Antonio(quello per anni d'esperienza e skill
+//in una determinata compagnia)(?)
 }
--- ProjectWorkBariBE/src/main/java/com/dao/JobOfferRepository.java
+++ ProjectWorkBariBE/src/main/java/com/dao/JobOfferRepository.java
@@ -1,5 +1,8 @@
 package com.dao;
 
+import java.sql.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,12 @@ import com.model.JobOffer;
 
 @Repository
 public interface JobOfferRepository extends JpaRepository<JobOffer, Integer> {
+	
+	List<JobOffer> findByTitle(String title);
+	List<JobOffer> findByStartDateAfterAndEndDateBefore(Date startDate, Date endDate);
+	List<JobOffer> findByCompanyClient_idCompanyClient(Integer idCompanyClient);
+	List<JobOffer> findByMinRalAfterAndMaxRalBefore(Integer minRal, Integer maxRal);
+	List<JobOffer> findByContractType_idContractType(Integer idContractType);
+	List<JobOffer> findByJobOfferSkills_Skill_Description(String description);//chiedere se description o title
 
 }
